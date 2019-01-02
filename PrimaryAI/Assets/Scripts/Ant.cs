23e10d9455e293f74a0519fc4111d171092792b3
Flzzyf <2981019122@qq.com>
2019-01-02 12:02:06 +0800

    初始跟随逻辑

---
+++ PrimaryAI/Assets/Scripts/Ant.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Ant : MonoBehaviour
+{
+    public float speed = 3;
+    public float rotateSpeed = 1;
+
+    public Vector2 idleTimeRange = new Vector2(2, 3);
+
+    public Vector2 rotateAngleRange = new Vector2(20, 90);
+
+    public Vector2 speedRange = new Vector2(2, 4);
+
+    float timer;
+
+    public float eyeSight = 3;
+
+    public Transform target;
+
+    public List<Ant> followers = new List<Ant>();
+
+    void Start()
+    {
+        //初始随机朝向
+        transform.Rotate(new Vector3(0, 0, Random.Range(0, 360)));
+
+        speed = Random.Range(speedRange.x, speedRange.y);
+    }
+    
+    void Update()
+    {
+        if(target != null)
+        {
+
+            return;
+        }
+
+        if(timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        else
+        {
+            timer = Random.Range(idleTimeRange.x, idleTimeRange.y);
+
+            //随机方向旋转
+            RandomRotate();
+        }
+
+        //看见其他人
+        RaycastHit2D[] hit = Physics2D.RaycastAll(transform.position, transform.up, eyeSight);
+        foreach (var item in hit)
+        {
+            if (item.collider.gameObject != gameObject)
+            {
+                if (item.collider.gameObject.tag == "Bot")
+                {
+                    FindTarget(item.collider.gameObject);
+                }
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //前进
+        transform.Translate(transform.up * speed * Time.deltaTime, Space.World);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, transform.position + transform.up * eyeSight);
+    }
+
+    //发现目标
+    void FindTarget(GameObject _target)
+    {
+        target = _target.transform;
+
+        if (_target.tag == "Bot")
+        {
+            transform.DORotate(_target.transform.eulerAngles, 1);
+
+            _target.GetComponent<Ant>().followers.Add(this);
+        }
+    }
+
+    //随机旋转
+    void RandomRotate()
+    {
+        int sign = Random.Range(0, 2) == 0 ? -1 : 1;
+        float rotateAngle = Random.Range(rotateAngleRange.x, rotateAngleRange.y);
+        float rotateTime = rotateAngle * rotateSpeed * Time.deltaTime;
+        Vector3 targetRotation = transform.eulerAngles + new Vector3(0, 0, sign * rotateAngle);
+
+        transform.DORotate(targetRotation, rotateTime);
+
+        print(rotateTime);
+
+        //如果有追随者，让其也跟着转
+        if(followers.Count > 0)
+        {
+            foreach (var item in followers)
+            {
+                item.RotateTo(targetRotation, rotateTime);
+            }
+        }
+    }
+
+    public void RotateTo(Vector3 _rotation, float _time)
+    {
+        transform.DORotate(_rotation, _time);
+    }
+}
---
+++ PrimaryAI/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject prefab_bots;
+
+    public float createRange = 10;
+
+    void Start()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Vector2 pos = new Vector2(Random.Range(-1, 1) * createRange, Random.Range(-1, 1) * createRange);
+            Instantiate(prefab_bots, pos, Quaternion.identity);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
